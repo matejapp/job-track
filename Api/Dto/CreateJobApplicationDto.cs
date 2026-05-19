88d6ff5b6b1eaf733ebc5e225846ff76a4352a48
Mateja Pavlovic <pavlovicinq@gmail.com>
2026-05-19 12:33:41 +0200

    Negative test

--- Api/Dto/CreateJobApplicationDto.cs
+++ Api/Dto/CreateJobApplicationDto.cs
@@ -5,7 +5,7 @@ namespace Api.Dto
     public class CreateJobApplicationDto
     {
 
-        public string CompanyName { get; set; } = String.Empty;
+        public string CompanyName { get; set; } = 1;
         public string Position { get; set; } = String.Empty;
         public string ApplicationLink { get; set; } = String.Empty;
         public ApplicationStatus Status { get; set; } = ApplicationStatus.Applied;
